13b2be4890dce8d1f3af1bc795771d7738037b76
jlepe <josemanuel.lepe@gmail.com>
2022-09-15 20:45:20 -0500

    No sé que hice  :D

--- COMPUTO_INTEGRADO/SIMON_DICE/src/main.c
+++ COMPUTO_INTEGRADO/SIMON_DICE/src/main.c
@@ -8,48 +8,39 @@
 typedef struct Data_t
 {
     gpio_num_t pinSalida;
-    uint32_t pinPosicionEnArreglo;
     bool pinEstado;
 } structLed;
 
 // DEFINICION PARA REFERENCIA DE ISR
 #define ESP_INR_FLAG_DEFAULT 0
+
 // DEFINICION PINES SALIDA
 #define LED_OUTPUT_PINES                   \
     {                                      \
         13, 12, 14, 27, 26, 25, 33, 32, 23 \
     }
-/*// DEFINICION PINES ENTRADA
-const uint8_t countRows = 4;
-const uint8_t countColumns = 4;
-const uint8_t rowsPins[countRows] = { 35, 34, 15, 2 };
-const uint8_t columnsPins[countColumns] = { 4, 16, 17, 5 };
-char keys[countRows][countColumns] = {
-  { '1','2','3', 'A' },
-  { '4','5','6', 'B' },
-  { '7','8','9', 'C' },
-  { '#','0','*', 'D' }
-};*/
+// DEFINICION PINES ENTRADA
 #define LED_INPUT_PINES                 \
     {                                   \
         35, 34, 15, 2, 4, 16, 17, 5, 18 \
     }
+
 // DELAY DE INTERRUPCION
 #define INTERRUPT_DELAY 250
 
 // ------------------ DECLARACION VARIABLES GLOBALES ------------------
 
-// ARREGLO DE ENTEROS QUE REPRESENTA LOS PINES DE SALIDA PARA LOS LEDS
-const int pinesSalidasLed[] = LED_OUTPUT_PINES;
-
 // ARREGLO DE ENTEROS QUE REPRESENTA LOS PINES DE ENTRADA PARA LOS LEDS
-const int pinesEntradasLed[] = LED_INPUT_PINES;
+const gpio_num_t pinesEntradaLed[] = LED_INPUT_PINES;
+
+// ARREGLO DE ENTEROS QUE REPRESENTA LOS PINES DE SALIDA PARA LOS LEDS
+const gpio_num_t pinesSalidaLed[] = LED_OUTPUT_PINES;
 
 // SE DECLARA UN ARREGLO DE LEDS DEL TAMAÑO DE CANTIDAD DE PINES
-structLed leds[sizeof(pinesSalidasLed) / sizeof(int)];
+structLed leds[sizeof(pinesSalidaLed) / sizeof(gpio_num_t)];
 
 // HANDLER TAREA DE REBOTE
-TaskHandle_t HandlersReboteBtn[sizeof(pinesEntradasLed) / sizeof(int)];
+TaskHandle_t HandlersReboteBtn[sizeof(pinesEntradaLed) / sizeof(gpio_num_t)];
 
 // HANDLER SEMAFORO (THREAD SAFE)
 SemaphoreHandle_t HandlerSemaphoreBtn = NULL;
@@ -66,43 +57,30 @@ void app_main(void)
     app_main_setup();
 
     // SE CREAN LAS TAREAS PARA REBOTE DE BOTON
-    for (int i = 0; i < sizeof(pinesEntradasLed) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(pinesEntradaLed) / sizeof(int); i++)
     {
         xTaskCreate(rebote_boton, "Task_Rebote_" + i, 4096, (void *)&leds[i], 1, &HandlersReboteBtn[i]);
     }
-    /*while (1)
-    {
-        for (int i = 0; i < sizeof(pinesEntradasLed) / sizeof(int); i++)
-        {
-            gpio_set_level(pinesSalidasLed[i], true);
-            vTaskDelay(200 / portTICK_PERIOD_MS);
-            printf("hola %i\n", pinesSalidasLed[i]);
-            gpio_set_level(pinesSalidasLed[i], false);
-            vTaskDelay(200 / portTICK_PERIOD_MS);
-            printf("adios %i\n", pinesSalidasLed[i]);
-        }
-    }*/
 }
 
 void app_main_setup()
 {
     // DECLARACION ENTRADAS
-    for (int i = 0; i < sizeof(pinesEntradasLed) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(pinesEntradaLed) / sizeof(gpio_num_t); i++)
     {
-        gpio_pad_select_gpio(pinesEntradasLed[i]);
-        gpio_set_direction(pinesEntradasLed[i], GPIO_MODE_INPUT);
+        gpio_pad_select_gpio(pinesEntradaLed[i]);
+        gpio_set_direction(pinesEntradaLed[i], GPIO_MODE_INPUT);
         // SE ACTIVA LA RESISTENCIA INTERNA EN MODO PULL-DOWN
-        gpio_pullup_en(pinesEntradasLed[i]);
+        gpio_pulldown_en(pinesEntradaLed[i]);
     }
 
     // DECLARACION SALIDAS
-    for (int i = 0; i < sizeof(pinesSalidasLed) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(pinesSalidaLed) / sizeof(gpio_num_t); i++)
     {
-        gpio_pad_select_gpio(pinesSalidasLed[i]);
-        gpio_set_direction(pinesSalidasLed[i], GPIO_MODE_OUTPUT);
+        gpio_pad_select_gpio(pinesSalidaLed[i]);
+        gpio_set_direction(pinesSalidaLed[i], GPIO_MODE_OUTPUT);
 
-        // (INICIALIZAR EL ESTADO EN TRUE PARA QUE AL INICIAR EL PROGRAMA ESTE CAMBIE A FALSE)
-        structLed data = {pinesSalidasLed[i], i, false};
+        structLed data = {pinesSalidaLed[i], false};
         leds[i] = data;
     }
 
@@ -110,19 +88,19 @@ void app_main_setup()
     gpio_install_isr_service(ESP_INR_FLAG_DEFAULT);
 
     // CONFIGURACION ISR <<INTERRUP SERVICE ROUTINE>>
-    for (int i = 0; i < sizeof(pinesEntradasLed) / sizeof(int); i++)
+    for (int i = 0; i < sizeof(pinesEntradaLed) / sizeof(gpio_num_t); i++)
     {
         // CONFIGURACION DEL TIPO DE EVENTO PARA TRIGGER DEL BOTON
         // EVENTO AL DEJAR DE PRESIONAR EL BOTON (GPIO_INTR_NEGEDGE)
         // EVENTO AL PRESIONAR EL BOTON (GPIO_INTR_POSEDGE)
-        gpio_set_intr_type(pinesEntradasLed[i], GPIO_INTR_POSEDGE);
-
-        // SE AGREGA EL HANDLER PARA EL ISR
-        gpio_isr_handler_add(pinesEntradasLed[i], isr_handler_boton, (int *)&pinesEntradasLed[i]);
+        gpio_set_intr_type(pinesEntradaLed[i], GPIO_INTR_POSEDGE);
 
         // SE CONFIGURA EL HANDLER PARA LA TAREA
         TaskHandle_t data = NULL;
         HandlersReboteBtn[i] = data;
+
+        // SE AGREGA EL HANDLER PARA EL ISR
+        gpio_isr_handler_add(pinesEntradaLed[i], isr_handler_boton, &HandlersReboteBtn[i]);
     }
 
     // SE CREA EL HANDLER DE TIPO SEMAFORO PARA MULTITASKING
@@ -130,39 +108,12 @@ void app_main_setup()
     // SE CEDE EL USO DEL HANDLER DE TIPO SEMAFORO
     xSemaphoreGive(HandlerSemaphoreBtn);
 }
-/*
-bool readKeypad()
-{
-  bool rst = false;
-  // Barrido de columnas
-  for (byte c = 0; c < countColumns; c++)
-  {
-    // Poner columna a LOW
-    pinMode(columnsPins[c],OUTPUT);
-    digitalWrite(columnsPins[c], LOW);
 
-    // Barrer todas las filas comprobando pulsaciones
-    for (byte r = 0; r < countRows; r++)
-    {
-      if (digitalRead(columnsPins[r]) == LOW)
-      {
-        // Pulsacion detectada, guardar fila y columna
-        iRow = r;
-        iCol = c;
-        rst = true;
-      }
-    }
-    // Devolver la columna a alta impedancia
-    digitalWrite(columnsPins[c], HIGH);
-    pinMode(columnsPins[c], INPUT);
-  }
-  return rst;
-}
-*/
 void IRAM_ATTR isr_handler_boton(void *arg)
 {
     TaskHandle_t *data = (TaskHandle_t *)arg;
 
+    printf("OvO %p \n", data);
     // SE REANUDA LA TAREA
     xTaskResumeFromISR(data);
 }
@@ -177,16 +128,17 @@ void rebote_boton(void *arg)
         // SE APROPIA DEL SEMAFORO PARA AVISAR QUE UTILIZARA LA VARIABLE DE LED
         xSemaphoreTakeFromISR(HandlerSemaphoreBtn, xHigherPriorityTaskWoken);
 
-        // UN DELAY PEQUEÑO PARA EVITAR CUALQUIER ERROR
-        vTaskDelay(INTERRUPT_DELAY / portTICK_PERIOD_MS);
         // CAMBIA EL ESTADO DEL LED
         data->pinEstado = !data->pinEstado;
         // SE ACTUALIZA EL ESTADO DEL LED
         gpio_set_level(data->pinSalida, data->pinEstado);
-        printf("Button %i pressed!\n", data->pinPosicionEnArreglo);
+        printf("Button %i pressed!\n", data->pinSalida);
 
         // SE CEDE EL USO DEL HANDLER DE TIPO SEMAFORO
         xSemaphoreGiveFromISR(HandlerSemaphoreBtn, xHigherPriorityTaskWoken);
+
+        // UN DELAY PEQUEÑO PARA EVITAR CUALQUIER ERROR
+        vTaskDelay(INTERRUPT_DELAY / portTICK_PERIOD_MS);
         // SE AUTOSUSPENDE LA TAREA
         vTaskSuspend(NULL);
     }
